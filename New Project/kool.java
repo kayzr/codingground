b609092e981b87817219a8f890292a11105600eb
kayzr <kaizertau@gmail.com>
2015-11-25 14:50:54 +0200

    Created by Coding Ground Client

---
+++ New Project/kool.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class kool{
+    
+    public String sans(){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        return str;
+    }
+    public double sansdoub(){
+        Scanner scan = new Scanner(System.in);
+        double dnum = scan.nextDouble();
+        return dnum;
+    }
+    public void adding(){
+        System.out.println("Bakng");
+    }
+}
